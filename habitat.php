3eb17579220cf5c8b311efa36cc03555e2464e8c
Jessycav <jessyca.vannier@gmail.com>
2024-10-07 04:53:06 +0200

    responsive

--- habitat.php
+++ habitat.php
@@ -10,25 +10,26 @@
         <h3>Découvrez les différents habitats de notre zoo Arcadia</h3>
         <section id="habitats" class="thumb">
             <div class="box-container">
-            <?php
-                $query = "SELECT habitat.habitat_id, habitat.habitat_name, habitat_image.habitat_image_url 
-                        FROM habitat 
-                        JOIN habitat_image 
-                        ON habitat.habitat_id = habitat_image.habitat_id";
-                $stmt = $conn->prepare($query);
-                $stmt->execute();
-                $habitats = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                <?php
+                    $query = "SELECT habitat.habitat_id, habitat.habitat_name, habitat_image.habitat_image_url 
+                            FROM habitat 
+                            JOIN habitat_image 
+                            ON habitat.habitat_id = habitat_image.habitat_id";
+                    $stmt = $conn->prepare($query);
+                    $stmt->execute();
+                    $habitats = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-                foreach ($habitats as $habitat) {
-                    echo "<div class='box'>";
-                    echo "<h4>" . htmlspecialchars($habitat['habitat_name']) . "</h4>";
-                    echo "<img src='" . htmlspecialchars($habitat['habitat_image_url'], ENT_QUOTES) . "' alt='" . htmlspecialchars($habitat['habitat_name'], ENT_QUOTES) . "'>";
-                    echo "<a href='habitat_detail.php?id=" . urlencode($habitat['habitat_id']) . "'>";
-                    echo "<button class='btn'>Voir le détail</button>";
-                    echo "</a>";
-                    echo "</div>";
-                }     
-            ?>
+                    foreach ($habitats as $habitat) {
+                        echo "<div class='box'>";
+                        echo "<h4>" . htmlspecialchars($habitat['habitat_name']) . "</h4>";
+                        echo "<img src='" . htmlspecialchars($habitat['habitat_image_url'], ENT_QUOTES) . "' alt='" . htmlspecialchars($habitat['habitat_name'], ENT_QUOTES) . "'>";
+                        echo "<a href='habitat_detail.php?id=" . urlencode($habitat['habitat_id']) . "'>";
+                        echo "<button class='btn'>Voir le détail</button>";
+                        echo "</a>";
+                        echo "</div>";
+                    }     
+                ?>
+            </div>
         </section>
 
             <!-- Footer -->
